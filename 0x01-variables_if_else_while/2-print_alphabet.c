2e4e121513ec467d3e2ba0f9db15446b52aff440
LewisJassy <jassylewis70@gmail.com>
2023-09-08 08:59:28 -0700

    Alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print a to z followed by a newline
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char i =  'a';
+	
+	while (putchar(i++) < 'z')
+		;
+	putchar('\n');
+	return (0);
+}
