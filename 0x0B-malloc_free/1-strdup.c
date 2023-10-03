cb98de36c1061ce6b1c10703c739edbc8296aa71
LewisJassy <jassylewis70@gmail.com>
2023-10-03 09:51:20 -0700

    No succcess without failure

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - copy `str' and return pointer
+ * @str: string to copy
+ *
+ * Return: pointer to copy of `str', or NULL on failure
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *dup;
+
+	if (str == NULL)
+		return (str);
+
+	for (i = 0; str[i]; ++i)
+		;
+	dup = (char *) malloc((i + 1) * sizeof(char));
+	if (dup == NULL)
+		return (dup);
+	for (j = 0; j <= i; ++j)
+		dup[j] = str[j];
+	return (dup);
+}
