d2211f561146f24fe6cd255387ee2c556a2da2ec
LEWIS <jassylewis70@gmail.com>
2023-10-11 16:32:26 +0300

    prints My_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ *print_name - printing name
+ *@name: string name
+ *@f : function pointer
+ */
+void print_name(char *name, void (*f)(char *)){
+	if (f == NULL) {
+		exit (EXIT_FAILURE);
+	}
+	f(name);
+}
