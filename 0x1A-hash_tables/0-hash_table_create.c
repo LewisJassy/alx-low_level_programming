43b2402522359eb33ebf3d7833f7525a710bfcb2
LewisJassy <jassylewis70@gmail.com>
2023-12-22 23:58:35 +0300

    remove comments

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,8 +4,8 @@
 hash_table_t *hash_table_create(unsigned long int size) {
     hash_table_t *array = (hash_table_t*)malloc(sizeof(hash_table_t));
     size_t i;
+
     
-    // Allocate memory for the array of pointers (buckets) inside the hash table
     hash_table_t* new_table = (hash_table_t*)malloc(sizeof(hash_table_t));
     if (size == 0){
         return NULL;
@@ -18,11 +18,11 @@ hash_table_t *hash_table_create(unsigned long int size) {
         return NULL;
     }
 
-    // Initiallize the hash table with the provided size
+    
     new_table->array = array;
     new_table->size = size;
 
-    // Intialize each bucket in the array to NULL
+    
     for (i  = 0; i < size; i++){
         *(array + 1) = NULL;
     }
