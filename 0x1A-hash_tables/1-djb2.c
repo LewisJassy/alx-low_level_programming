0016f0ad04a039911260a33b9a34c6226b03e1ec
LewisJassy <jassylewis70@gmail.com>
2023-12-23 00:51:00 +0300

    bug fixes

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -2,11 +2,11 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 
-unsigned long int hash_djb2(const unsigned char *str) {
+unsigned long int hash_djb2(const unsigned char *str) 
+{
     unsigned long int hash = 5381;
     int c;
-    while ((c = *str++)) {
+    while ((c = *str++)) 
         hash = ((hash << 5) + hash) + c;
-    }
-    return hash;
+    return (hash);
 }
