325dfeb88fc2013b7aa3f3ab13a36e66bc73f82e
LewisJassy <jassylewis70@gmail.com>
2023-12-23 09:47:52 +0300

    whitespace issue

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 /**
- * hash_table_get - function to retrieve value associated with `key`.
- * @ht: pointer to hash table
- * @key: key with which to retrieve value
+ * hash_table_get - gets a hash table key value pair in hash table
+ * @ht: hash table to pull value from
+ * @key: key to generate hash value and index off
  *
- * Return: value associated with `key`, or NULL if `key` does not exist.
+ * Return: value associated with key, or NULL if not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -14,15 +14,21 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	char *value = NULL;
 	hash_node_t *poss_nodes;
 
-	if (ht == NULL || key == NULL)
+	if (!ht || !key)
 		return (NULL);
-	index = key_index((const unsigned char *) key, ht->size);
-	bucket = *(ht->array + index);
-	while (bucket)
+	else if (strlen(key) == 0)
+		return (NULL);
+
+	key_i = key_index((unsigned char *)key, ht->size);
+	if ((ht->array)[key_i] == NULL)
+		return (NULL);
+
+	poss_nodes = ht->array[key_i];
+	while (!value)
 	{
-		if (strcmp(key, bucket->key) == 0)
-			return (bucket->value);
-		bucket = bucket->next;
+		if (strcmp(poss_nodes->key, key) == 0)
+			value = poss_nodes->value;
+		poss_nodes = poss_nodes->next;
 	}
-	return (NULL);
+	return (value);
 }
